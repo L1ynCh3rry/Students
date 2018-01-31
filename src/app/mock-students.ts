0a165a2a3dd492d06aaf53e578ee0ab9ce634588
unknown <luka1cosic67@gmail.com>
2018-02-01 00:23:36 +0100

    css

--- src/app/mock-students.ts
+++ src/app/mock-students.ts
@@ -3,14 +3,14 @@ import { Student } from './student';
 
 
 export const STUDENTS: Student[] = [
-  { id: 1, name: 'Milos Stojiljkovic' },
-  { id: 2, name: 'Nebojsa Konstantinovic' },
-  { id: 3, name: 'Dusan Ivanovic' },
-  { id: 4, name: 'Nikola Dosenovic' },
-  { id: 5, name: 'Borivoje Skrbina' },
-  { id: 6, name: 'Anka Markovic-Borak' },
-  { id: 7, name: 'Ivana Petrovic' },
-  { id: 8, name: 'Jovana Nikolic' },
-  { id: 9, name: 'Luka Cosic' },
-  { id: 10, name: 'Marija Jeremic' }
+  { id: 1, name: 'Milos Stojiljkovic', gender: 'male' },
+  { id: 2, name: 'Nebojsa Konstantinovic', gender: 'male' },
+  { id: 3, name: 'Dusan Ivanovic', gender: 'male' },
+  { id: 4, name: 'Nikola Dosenovic', gender: 'male' },
+  { id: 5, name: 'Borivoje Skrbina', gender: 'male' },
+  { id: 6, name: 'Anka Markovic-Borak', gender: 'female' },
+  { id: 7, name: 'Ivana Petrovic', gender: 'female' },
+  { id: 8, name: 'Jovana Nikolic', gender: 'female' },
+  { id: 9, name: 'Luka Cosic', gender: 'male' },
+  { id: 10, name: 'Marija Jeremic', gender: 'female' }
 ];
