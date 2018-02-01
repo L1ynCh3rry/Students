073d031f8306bb2b5f297b4cc2f27596d7a5c9de
Frontend <frontend@Cubess-iMac-2.local>
2018-02-01 14:06:12 +0100

    Added descripton of student

--- src/app/mock-students.ts
+++ src/app/mock-students.ts
@@ -3,14 +3,14 @@ import { Student } from './student';
 
 
 export const STUDENTS: Student[] = [
-  { id: 1, name: 'Milos Stojiljkovic', gender: 'male' },
-  { id: 2, name: 'Nebojsa Konstantinovic', gender: 'male' },
-  { id: 3, name: 'Dusan Ivanovic', gender: 'male' },
-  { id: 4, name: 'Nikola Dosenovic', gender: 'male' },
-  { id: 5, name: 'Borivoje Skrbina', gender: 'male' },
-  { id: 6, name: 'Anka Markovic-Borak', gender: 'female' },
-  { id: 7, name: 'Ivana Petrovic', gender: 'female' },
-  { id: 8, name: 'Jovana Nikolic', gender: 'female' },
-  { id: 9, name: 'Luka Cosic', gender: 'male' },
-  { id: 10, name: 'Marija Jeremic', gender: 'female' }
+  { id: 1, name: 'Milos Stojiljkovic', gender: 'male', description: 'deskripcija 1' },
+  { id: 2, name: 'Nebojsa Konstantinovic', gender: 'male', description: 'deskripcija 2' },
+  { id: 3, name: 'Dusan Ivanovic', gender: 'male', description: 'deskripcija 3' },
+  { id: 4, name: 'Nikola Dosenovic', gender: 'male', description: 'deskripcija 4' },
+  { id: 5, name: 'Borivoje Skrbina', gender: 'male', description: 'deskripcija 5' },
+  { id: 6, name: 'Anka Markovic-Borak', gender: 'female', description: 'deskripcija 6' },
+  { id: 7, name: 'Ivana Petrovic', gender: 'female', description: 'deskripcija 7' },
+  { id: 8, name: 'Jovana Nikolic', gender: 'female', description: 'deskripcija 8' },
+  { id: 9, name: 'Luka Cosic', gender: 'male', description: 'deskripcija 9' },
+  { id: 10, name: 'Marija Jeremic', gender: 'female', description: 'deskripcija 10' }
 ];
